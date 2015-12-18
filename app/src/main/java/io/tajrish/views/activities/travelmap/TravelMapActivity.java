aeea27ced9b3d05c8c1863b572b9beaa7b0160ee
Sadjad Fallah <m.sadjad.fallah@gmail.com>
2015-12-18 08:43:49 +0330

    Paper Up

--- app/src/main/java/io/tajrish/views/activities/travelmap/TravelMapActivity.java
+++ app/src/main/java/io/tajrish/views/activities/travelmap/TravelMapActivity.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Display;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdate;
@@ -26,6 +30,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import github.chenupt.dragtoplayout.DragTopLayout;
 import io.tajrish.R;
 import io.tajrish.models.Pin;
 import io.tajrish.models.Province;
@@ -43,6 +48,11 @@ public class TravelMapActivity extends BaseActivity {
     private ArrayList<Marker> markers = new ArrayList<>();
     private Map<Marker, Integer> markersMap = new HashMap();
 
+    private FrameLayout mExpander;
+    private ImageView mBackpack;
+    private boolean isOpen = true;
+    private int mHeight;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,6 +66,46 @@ public class TravelMapActivity extends BaseActivity {
 //        intent.putExtra(PACKAGE_NAME + ": longitude", location[1]);
         getJsonInformationFromIntent();
         setUpMapIfNeeded();
+        findViewsById();
+        getScreenHeight();
+        setOnClickListeners();
+    }
+
+    private void findViewsById() {
+        mBackpack = (ImageView) findViewById(R.id.travel_backpack);
+        mExpander = (FrameLayout) findViewById(R.id.travel_expander);
+    }
+
+    private void getScreenHeight() {
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        mHeight = size.y;
+    }
+
+    private void setOnClickListeners() {
+        mBackpack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isOpen) {
+                    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                            FrameLayout.LayoutParams.MATCH_PARENT,
+                            RadioGroup.LayoutParams.MATCH_PARENT
+                    );
+                    params.setMargins(0, mHeight-200, 0, 0);
+                    mExpander.setLayoutParams(params);
+                } else {
+                    FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                            FrameLayout.LayoutParams.MATCH_PARENT,
+                            RadioGroup.LayoutParams.MATCH_PARENT
+                    );
+                    params.setMargins(0, 30, 0, 0);
+                    mExpander.setLayoutParams(params);
+                }
+                isOpen = !isOpen;
+                Log.d(PACKAGE_NAME, isOpen ? "TRUE" : "FALSE");
+            }
+        });
     }
 
     private void getJsonInformationFromIntent() {
